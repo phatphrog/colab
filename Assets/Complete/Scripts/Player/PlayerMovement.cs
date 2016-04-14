12ad1673bc1c727099496e908a4530b5ea12c197
Marcel Terblanche <phatphrog@gmail.com>
2016-04-14 15:07:57 +0200

    updated dust trail on players. removed audio from player movement

--- Assets/Complete/Scripts/Player/PlayerMovement.cs
+++ Assets/Complete/Scripts/Player/PlayerMovement.cs
@@ -7,10 +7,6 @@ namespace Complete
         public int m_PlayerNumber = 1;              // Used to identify which avatar belongs to which player.  This is set by this avatar's manager.
         public float m_Speed = 3f;                 // How fast the player moves forward and back.
         public float m_TurnSpeed = 180f;            // How fast the player turns in degrees per second.
-        public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
-        public AudioClip m_EngineIdling;            // Audio to play when the player isn't moving.
-        public AudioClip m_EngineDriving;           // Audio to play when the player is moving.
-        public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
         public bool scaled = false;
         public bool explode = false;
         public Vector3 currentPos;
@@ -56,9 +52,6 @@ namespace Complete
             // The axes names are based on player number.
             m_VerticalAxisName = "Vertical" + m_PlayerNumber;
             m_HorizontalAxisName = "Horizontal" + m_PlayerNumber;
-
-            // Store the original pitch of the audio source.
-            m_OriginalPitch = m_MovementAudio.pitch;
         }
 
 
@@ -86,35 +79,6 @@ namespace Complete
             }
         }
 
-
-        private void EngineAudio ()
-        {
-            // If there is no input (the player is stationary)...
-            if (Mathf.Abs (m_VerticalInputValue) < 0.1f && Mathf.Abs (m_HorizontalInputValue) < 0.1f)
-            {
-                // ... and if the audio source is currently playing the driving clip...
-                if (m_MovementAudio.clip == m_EngineDriving)
-                {
-                    // ... change the clip to idling and play it.
-                    m_MovementAudio.clip = m_EngineIdling;
-                    m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                    m_MovementAudio.Play ();
-                }
-            }
-            else
-            {
-                // Otherwise if the player is moving and if the idling clip is currently playing...
-                if (m_MovementAudio.clip == m_EngineIdling)
-                {
-                    // ... change the clip to driving and play.
-                    m_MovementAudio.clip = m_EngineDriving;
-                    m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                    m_MovementAudio.Play();
-                }
-            }
-        }
-
-
         private void FixedUpdate ()
         {
             // Adjust the rigidbodies position and orientation in FixedUpdate.
@@ -141,17 +105,5 @@ namespace Complete
             m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
         }
 
-        /*
-        private void Turn ()
-        {
-            // Determine the number of degrees to be turned based on the input, speed and time between frames.
-            float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
-
-            // Make this into a rotation in the y axis.
-            Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
-
-            // Apply this rotation to the rigidbody's rotation.
-            m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
-        }*/
     }
 }
