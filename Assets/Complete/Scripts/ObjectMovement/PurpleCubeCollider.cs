95636d30f3485d2290c94e4c50f1a4ce7e7061f5
Marcel Terblanche <phatphrog@gmail.com>
2016-05-03 16:53:26 +0200

    continuation of room 5

---
+++ Assets/Complete/Scripts/ObjectMovement/PurpleCubeCollider.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PurpleCubeCollider : MonoBehaviour {
+
+    public bool isLarge = false;
+    private bool player1Collision = false;
+    private bool player2Collision = false;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        PurpleCollision(collision);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        PurpleCollision(collision);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && isLarge)
+        {
+            collision.rigidbody.velocity = Vector3.zero;
+
+            Complete.PlayerMovement script = collision.gameObject.GetComponent<Complete.PlayerMovement>();
+            if (script)
+            {
+                if (script.playerNumber == 2)
+                {
+                    player2Collision = false;
+                }
+                else if (script.playerNumber == 1)
+                {
+                    player1Collision = false;
+                }
+
+            }
+        }
+    }
+
+    private void PurpleCollision(Collision collision)
+    {
+        Complete.PlayerSpecial specialScript = collision.gameObject.GetComponent<Complete.PlayerSpecial>();
+        if (specialScript)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                if (!isLarge)
+                {
+                    GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+                }
+                else
+                {
+                    if (specialScript.playerNumber == 2)
+                    {
+                        player2Collision = true;
+                    }
+                    else if (specialScript.playerNumber == 1)
+                    {
+                        player1Collision = true;
+                    }
+
+
+                    if (player2Collision && player1Collision)
+                    {
+                        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionY;
+                    }
+                    else
+                    {
+                        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+                    }
+                }
+
+            }
+            else
+            {
+                GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            }
+        }
+    }
+}
