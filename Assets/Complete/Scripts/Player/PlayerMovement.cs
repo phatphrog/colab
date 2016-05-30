fc95704bafd6ac1be104ea4af69ab2e9cc8e35aa
Marcel Terblanche <phatphrog@gmail.com>
2016-05-30 17:16:29 +0200

    minor bugfixes

--- Assets/Complete/Scripts/Player/PlayerMovement.cs
+++ Assets/Complete/Scripts/Player/PlayerMovement.cs
@@ -98,7 +98,7 @@ namespace Complete
                 float fracComplete = (Time.time - startTime) / translationTime;
                 transform.position = Vector3.Slerp(riseRelCenter, setRelCenter, fracComplete);
                 transform.position += center;
-                if(transform.position.x == endPos.x && transform.position.z == endPos.z)
+                if(transform.position == endPos)
                 {
                     explode = false;
                 }
