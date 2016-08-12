69be23ecb61866cd3ab08a2a3be9c460e319122f
Marcel Terblanche <phatphrog@gmail.com>
2016-08-12 16:52:46 +0200

    added basic StartMenuScene and increased timings slightly for timed events in game

--- Assets/Complete/Scripts/Managers/GameManager.cs
+++ Assets/Complete/Scripts/Managers/GameManager.cs
@@ -107,7 +107,7 @@ namespace Complete
 
             // Increment the round number and display text showing the players what round it is.
             m_RoundNumber++;
-            m_MessageText.text = "Loading...";
+           // m_MessageText.text = "Loading...";
 
             // Wait for the specified length of time until yielding control back to the game loop.
             yield return m_StartWait;
---
+++ Assets/Complete/Scripts/UI/MenuOptions.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuOptions : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startButton;
+    public Button exitButton;
+    public Canvas startMenu;
+
+	// Use this for initialization
+	void Start () 
+    {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startButton = startButton.GetComponent<Button>();
+        exitButton = exitButton.GetComponent<Button>();
+        startMenu = this.GetComponent<Canvas>();
+
+        quitMenu.enabled = false;
+
+	}
+	
+	public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        startButton.enabled = false;
+        exitButton.enabled = false;
+        startMenu.enabled = false;
+
+    }
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        startButton.enabled = true;
+        exitButton.enabled = true;
+        startMenu.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("CompleteMainScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
